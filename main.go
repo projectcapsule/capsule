feec653db42693ea2ad2e57632b47f5a1f3dfa90
Dario Tranchitella <dario@tranchitella.eu>
2020-09-23 09:21:03 +0200

    No need to add a duplicated schema (#100)

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ import (
 	"regexp"
 	goRuntime "runtime"
 
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -61,7 +60,6 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(capsulev1alpha1.AddToScheme(scheme))
-	utilruntime.Must(corev1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
