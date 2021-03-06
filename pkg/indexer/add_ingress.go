e0719504ef8dd61c6781ab3df5160fe305a2015e
Dario Tranchitella <dario@tranchitella.eu>
2021-03-06 12:27:35 +0100

    bug: disabling ingresses.networking.k8s.io indexers on k8s < 1.19

--- pkg/indexer/add_ingress.go
+++ pkg/indexer/add_ingress.go
@@ -22,10 +22,17 @@ import (
 	networkingv1beta1 "k8s.io/api/networking/v1beta1"
 
 	"github.com/clastix/capsule/pkg/indexer/ingress"
+	"github.com/clastix/capsule/pkg/webhook/utils"
 )
 
 func init() {
 	AddToIndexerFuncs = append(AddToIndexerFuncs, ingress.Hostname{Obj: &extensionsv1beta1.Ingress{}})
-	AddToIndexerFuncs = append(AddToIndexerFuncs, ingress.Hostname{Obj: &networkingv1.Ingress{}})
+	// ingresses.networking.k8s.io/v1 introduced by 1.19
+	{
+		majorVer, minorVer, _, _ := utils.GetK8sVersion()
+		if majorVer == 1 && minorVer >= 19 {
+			AddToIndexerFuncs = append(AddToIndexerFuncs, ingress.Hostname{Obj: &networkingv1.Ingress{}})
+		}
+	}
 	AddToIndexerFuncs = append(AddToIndexerFuncs, ingress.Hostname{Obj: &networkingv1beta1.Ingress{}})
 }
