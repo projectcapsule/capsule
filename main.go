8fb4b7d4a1ee2e27efecd27e4dc0aefb0d0d9c2b
Dario Tranchitella <dario@tranchitella.eu>
2021-06-27 23:19:19 +0200

    feat: scaffolding v1beta1 Tenant version

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	capsulev1alpha1 "github.com/clastix/capsule/api/v1alpha1"
+	capsulev1beta1 "github.com/clastix/capsule/api/v1beta1"
 	"github.com/clastix/capsule/controllers"
 	config "github.com/clastix/capsule/controllers/config"
 	"github.com/clastix/capsule/controllers/rbac"
@@ -50,6 +51,7 @@ func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(capsulev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(capsulev1beta1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
