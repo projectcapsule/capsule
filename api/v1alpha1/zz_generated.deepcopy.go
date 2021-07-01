6595bc1fcc413fe6bd77a6572ecf253af0c796ea
Maksim Fedotov <m_fedotov@wargaming.net>
2021-07-01 17:17:35 +0300

    chore: v1beta1 goimports and formatting

--- api/v1alpha1/zz_generated.deepcopy.go
+++ api/v1alpha1/zz_generated.deepcopy.go
@@ -10,7 +10,7 @@ package v1alpha1
 import (
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
-	"k8s.io/api/rbac/v1"
+	v1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
