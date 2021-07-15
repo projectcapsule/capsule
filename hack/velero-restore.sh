cdf3bdd6d5371e10f1f0caea15e64f265422ab2d
alegrey91 <ale_grey_91@hotmail.it>
2021-07-15 16:29:23 +0200

    feat(velero): improve usage function

--- hack/velero-restore.sh
+++ hack/velero-restore.sh
@@ -23,11 +23,13 @@ TENANTS=""
 usage () {
    printf "Usage: $0 [flags] commands\n" 
    printf "Flags:\n" 
-   printf "\t-c, --kubeconfig\tPath to the kubeconfig file to use for CLI requests.\n" 
+   printf "\t-c, --kubeconfig /path/to/config\tPath to the kubeconfig file to use for CLI requests.\n" 
+   printf "\t-t, --tenant \"gas oil\"\t\tSpecify one or more tenants to be restored.\n" 
    printf "Commands:\n" 
    printf "\trestore\t\t\tPerform the restore on the cluster, patching the right object fields.\n" 
    printf "\n"
    printf "E.g. [restore]:\t$0 -c /path/to/kubeconfig restore\n"
+   printf "E.g. [restore]:\t$0 -t \"oil\" restore\n"
 }
 
 # Update KUBEOPTIONS global var.
