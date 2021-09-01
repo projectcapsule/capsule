196e3c910d086f1bccc69413b66c5c3935dbe690
alegrey91 <ale_grey_91@hotmail.it>
2021-09-01 12:45:13 +0200

    feat: add deny-wildcard annotation

---
+++ api/v1beta1/deny_wildcard.go
@@ -0,0 +1,15 @@
+// Copyright 2020-2021 Clastix Labs
+// SPDX-License-Identifier: Apache-2.0
+
+package v1beta1
+
+const (
+	denyWildcard = "capsule.clastix.io/deny-wildcard"
+)
+
+func (t *Tenant) IsWildcardDenied() bool {
+	if v, ok := t.Annotations[denyWildcard]; ok && v == "true" {
+		return true
+	}
+	return false
+}
