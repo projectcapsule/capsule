26965a5ea2b5fdae16f96f65bdd44ca1f186995c
Dario Tranchitella <dario@tranchitella.eu>
2021-09-14 18:03:40 +0200

    fix: skipping indexer if error is a NoKindMatch

--- pkg/indexer/indexer.go
+++ pkg/indexer/indexer.go
@@ -5,17 +5,20 @@ package indexer
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
 	networkingv1 "k8s.io/api/networking/v1"
 	networkingv1beta1 "k8s.io/api/networking/v1beta1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"github.com/clastix/capsule/pkg/indexer/ingress"
 	"github.com/clastix/capsule/pkg/indexer/namespace"
 	"github.com/clastix/capsule/pkg/indexer/tenant"
-	"github.com/clastix/capsule/pkg/webhook/utils"
 )
 
 type CustomIndexer interface {
@@ -24,26 +27,24 @@ type CustomIndexer interface {
 	Func() client.IndexerFunc
 }
 
-func AddToManager(ctx context.Context, mgr manager.Manager) error {
-	indexers := append([]CustomIndexer{},
+func AddToManager(ctx context.Context, log logr.Logger, mgr manager.Manager) error {
+	indexers := []CustomIndexer{
 		tenant.NamespacesReference{},
 		tenant.OwnerReference{},
 		namespace.OwnerReference{},
-	)
-
-	majorVer, minorVer, _, _ := utils.GetK8sVersion()
-	if majorVer == 1 && minorVer < 22 {
-		indexers = append(indexers,
-			ingress.HostnamePath{Obj: &extensionsv1beta1.Ingress{}},
-			ingress.HostnamePath{Obj: &networkingv1beta1.Ingress{}},
-		)
-	}
-	if majorVer == 1 && minorVer >= 19 {
-		indexers = append(indexers, ingress.HostnamePath{Obj: &networkingv1.Ingress{}})
+		ingress.HostnamePath{Obj: &extensionsv1beta1.Ingress{}},
+		ingress.HostnamePath{Obj: &networkingv1beta1.Ingress{}},
+		ingress.HostnamePath{Obj: &networkingv1.Ingress{}},
 	}
 
 	for _, f := range indexers {
 		if err := mgr.GetFieldIndexer().IndexField(ctx, f.Object(), f.Field(), f.Func()); err != nil {
+			missingAPIError := &meta.NoKindMatchError{}
+			if errors.As(err, &missingAPIError) {
+				log.Info(fmt.Sprintf("skipping setup of Indexer %T for object %T", f, f.Object()), "error", err.Error())
+
+				continue
+			}
 			return err
 		}
 	}
