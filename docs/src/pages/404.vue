7c5a1afe921ea30dd61ae8ded556d0e0336870f7
Luca Spezzano <spezzanoluca@gmail.com>
2021-11-02 20:28:58 +0100

    feat(docs): created 404 default page

---
+++ docs/src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <layout-default>
+    <div class="mx-auto space-y-4 py-16 text-center">
+      <h1 class="text-2xl sm:text-4xl font-semibold">404 - Page not found</h1>
+      <p>We couldn't find the page you are looking for</p>
+      <app-button link="/">Home</app-button>
+    </div>
+  </layout-default>
+</template>
+<script>
+import AppButton from "~/components/AppButton.vue";
+
+export default {
+   metaInfo() {
+    return {
+      title: "404 - Page not found",
+    };
+  },
+
+  components: {
+    AppButton,
+  },
+};
+</script>
