d027887c7f9ada5fc00f7a817afdc7004d0cde6d
Maksim Fedotov <m_fedotov@wargaming.net>
2022-05-16 20:13:18 +0300

    feat: protected tenant annotation

---
+++ e2e/tenant_protected_webhook_test.go
@@ -0,0 +1,40 @@
+//go:build e2e
+
+// Copyright 2020-2021 Clastix Labs
+// SPDX-License-Identifier: Apache-2.0
+
+package e2e
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	capsulev1beta1 "github.com/clastix/capsule/api/v1beta1"
+)
+
+var _ = Describe("Deleting a tenant with protected annotation", func() {
+	tnt := &capsulev1beta1.Tenant{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "protected_tenant",
+			Annotations: map[string]string{
+				capsulev1beta1.ProtectedTenantAnnotation: "",
+			},
+		},
+		Spec: capsulev1beta1.TenantSpec{
+			Owners: capsulev1beta1.OwnerListSpec{
+				{
+					Name: "john",
+					Kind: "User",
+				},
+			},
+		},
+	}
+
+	It("should fail", func() {
+		Expect(k8sClient.Create(context.TODO(), tnt)).Should(Succeed())
+		Expect(k8sClient.Delete(context.TODO(), tnt)).ShouldNot(Succeed())
+	})
+})
