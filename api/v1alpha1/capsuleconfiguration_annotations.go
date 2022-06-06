595a3e8e2b7b6343c55c61361dd7ca521db0df51
Maksim Fedotov <m_fedotov@wargaming.net>
2022-06-06 18:59:44 +0300

    chore: remove unused CASecretNameAnnotation constant

--- api/v1alpha1/capsuleconfiguration_annotations.go
+++ api/v1alpha1/capsuleconfiguration_annotations.go
@@ -5,7 +5,6 @@ const (
 	ForbiddenNodeLabelsRegexpAnnotation      = "capsule.clastix.io/forbidden-node-labels-regexp"
 	ForbiddenNodeAnnotationsAnnotation       = "capsule.clastix.io/forbidden-node-annotations"
 	ForbiddenNodeAnnotationsRegexpAnnotation = "capsule.clastix.io/forbidden-node-annotations-regexp"
-	CASecretNameAnnotation                   = "capsule.clastix.io/ca-secret-name"
 	TLSSecretNameAnnotation                  = "capsule.clastix.io/tls-secret-name"
 	MutatingWebhookConfigurationName         = "capsule.clastix.io/mutating-webhook-configuration-name"
 	ValidatingWebhookConfigurationName       = "capsule.clastix.io/validating-webhook-configuration-name"
