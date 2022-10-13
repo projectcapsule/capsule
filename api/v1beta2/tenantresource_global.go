318ad3e4d3368f81deefede06c20f5d739056500
Dario Tranchitella <dario@tranchitella.eu>
2022-10-13 15:51:43 +0200

    feat(api): globaltenantresource and tenantresource support

---
+++ api/v1beta2/tenantresource_global.go
@@ -0,0 +1,49 @@
+// Copyright 2020-2021 Clastix Labs
+// SPDX-License-Identifier: Apache-2.0
+
+package v1beta2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GlobalTenantResourceSpec defines the desired state of GlobalTenantResource.
+type GlobalTenantResourceSpec struct {
+	// Defines the Tenant selector used target the tenants on which resources must be propagated.
+	TenantSelector     metav1.LabelSelector `json:"tenantSelector,omitempty"`
+	TenantResourceSpec `json:",inline"`
+}
+
+// GlobalTenantResourceStatus defines the observed state of GlobalTenantResource.
+type GlobalTenantResourceStatus struct {
+	// List of Tenants addressed by the GlobalTenantResource.
+	SelectedTenants []string `json:"selectedTenants"`
+	// List of the replicated resources for the given TenantResource.
+	ProcessedItems []ObjectReferenceStatus `json:"processedItems"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+
+// GlobalTenantResource allows to propagate resource replications to a specific subset of Tenant resources.
+type GlobalTenantResource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GlobalTenantResourceSpec   `json:"spec,omitempty"`
+	Status GlobalTenantResourceStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// GlobalTenantResourceList contains a list of GlobalTenantResource.
+type GlobalTenantResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GlobalTenantResource `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&GlobalTenantResource{}, &GlobalTenantResourceList{})
+}
