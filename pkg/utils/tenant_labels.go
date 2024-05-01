8e47562b3d43fdea7ebdf4a56fc027f97b515769
Oliver Bähler <oliverbaehler@hotmail.com>
2024-05-01 10:08:14 +0200

    chore(lint): remove linter
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- pkg/utils/tenant_labels.go
+++ pkg/utils/tenant_labels.go
@@ -15,7 +15,6 @@ import (
 	"github.com/projectcapsule/capsule/api/v1beta2"
 )
 
-//nolint:typecheck
 func GetTypeLabel(t runtime.Object) (label string, err error) {
 	switch v := t.(type) {
 	case *v1beta1.Tenant, *v1beta2.Tenant:
