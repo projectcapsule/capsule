05f27fd43a1ca7bfc05f45fb29252650a29ec1ce
Hristo Hristov <me@hhristov.info>
2025-04-29 16:14:44 +0300

    feat(tenant): improve cordoning selection
    Signed-off-by: Hristo Hristov <me@hhristov.info>

---
+++ pkg/webhook/namespace/cordoning.go
@@ -0,0 +1,38 @@
+package namespace
+
+import (
+	"context"
+	"github.com/projectcapsule/capsule/pkg/configuration"
+	capsulewebhook "github.com/projectcapsule/capsule/pkg/webhook"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+type cordoningHandler struct {
+	cfg configuration.Configuration
+}
+
+func CordoningHandler(cfg configuration.Configuration) capsulewebhook.Handler {
+	return &cordoningHandler{
+		cfg: cfg,
+	}
+}
+
+func (h *cordoningHandler) OnCreate(client.Client, admission.Decoder, record.EventRecorder) capsulewebhook.Func {
+	return func(context.Context, admission.Request) *admission.Response {
+		return nil
+	}
+}
+
+func (h *cordoningHandler) OnUpdate(client.Client, admission.Decoder, record.EventRecorder) capsulewebhook.Func {
+	return func(context.Context, admission.Request) *admission.Response {
+		return nil
+	}
+}
+
+func (h *cordoningHandler) OnDelete(client.Client, admission.Decoder, record.EventRecorder) capsulewebhook.Func {
+	return func(context.Context, admission.Request) *admission.Response {
+		return nil
+	}
+}
