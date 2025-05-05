6f9cbbcccc75a45a8fa560beb063abc0648028e3
Hristo Hristov <me@hhristov.info>
2025-05-05 20:38:55 +0300

    feat(tenant): improve cordoning selection
    feat(tenant): improve cordoning selection
    Signed-off-by: Hristo Hristov <me@hhristov.info>
    
    Co-authored-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- pkg/webhook/namespace/mutation/cordoning.go
+++ pkg/webhook/namespace/mutation/cordoning.go
@@ -73,7 +73,7 @@ func (h *cordoningLabelHandler) syncNamespaceCordonLabel(ctx context.Context, c 
 	}
 
 	labels := ns.GetLabels()
-	if _, ok := labels[capsuleutils.CordonedLabel]; !ok {
+	if _, ok := labels[capsuleutils.CordonedLabel]; ok {
 		return nil
 	}
 
