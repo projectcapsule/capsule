5763fe0c654147f442fe000f6aab6ce91132124a
Hristo Hristov <me@hhristov.info>
2025-05-16 12:27:31 +0300

    feat(tenant): support gateway/class
    Signed-off-by: Hristo Hristov <me@hhristov.info>

---
+++ api/v1beta2/gateway_options.go
@@ -0,0 +1,12 @@
+// Copyright 2020-2023 Project Capsule Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1beta2
+
+import (
+	"github.com/projectcapsule/capsule/pkg/api"
+)
+
+type GatewayOptions struct {
+	AllowedClasses *api.DefaultAllowedListSpec `json:"allowedClasses,omitempty"`
+}
