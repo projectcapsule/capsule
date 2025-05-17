c0d127b038bb892627e6f8391da0d5568140e4cd
niuxe <kristian.d.kristensen@gmail.com>
2025-05-17 12:22:41 +0200

    feat: Permission CRD
    Signed-off-by: niuxe <kristian.d.kristensen@gmail.com>

--- api/v1beta2/permission.go
+++ api/v1beta2/permission.go
@@ -4,25 +4,10 @@
 package v1beta2
 
 import (
-	rbacv1 "k8s.io/api/rbac/v1"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type ExtendedSubject struct {
-	rbacv1.Subject
-	// `json:"actAsOwner,omitempty"
-	//  +kubebuilder:default=false`
-	ActAsOwner bool `json:"actAsOwner,omitempty"`
-}
-
 type PermissionSpec struct {
 	// Defines roleBindings between the ClusterRole and the Subjet in the Tenants namespaces.
-	// +kubebuilder:default={admin,capsule-namespace-deleter}
-	RoleBindings []string `json:"roleBindings,omitempty"`
-	// Defines additional cluster-role-bindings the specific subject should be assigned.
-	ClusterRoleBindings []string `json:"clusterRoleBindings,omitempty"`
-	// kubebuilder:validation:Minimum=1
-	Subjects          []ExtendedSubject     `json:"subjects"`
-	NamespaceSelector *metav1.LabelSelector `json:"namespaceSelector,omitempty"`
+	AllowedClusterBindings []metav1.LabelSelectorRequirement `json:"allowedClusterBindings,omitempty"`
 }
