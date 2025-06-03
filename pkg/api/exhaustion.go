a7447c004f92bab18bac0aa850ce099e1200c899
Oliver Bähler <oliverbaehler@hotmail.com>
2025-06-03 08:06:22 +0200

    feat(resourcepools): add improved metrics
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

---
+++ pkg/api/exhaustion.go
@@ -0,0 +1,16 @@
+// Copyright 2020-2023 Project Capsule Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+package api
+
+import (
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+// +kubebuilder:object:generate=true
+type PoolExhaustionResource struct {
+	// Available Resources to be claimed
+	Available resource.Quantity `json:"available,omitempty"`
+	// Requesting Resources
+	Requesting resource.Quantity `json:"requesting,omitempty"`
+}
