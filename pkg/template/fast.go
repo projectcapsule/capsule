4af9430e8a5d2af6d2f0375d8e6d937e8fce6626
Oliver Bähler <oliverbaehler@hotmail.com>
2025-12-11 16:25:43 +0100

    fix(controller): allow no spaces in template references
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- pkg/template/fast.go
+++ pkg/template/fast.go
@@ -12,19 +12,23 @@ import (
 	capsulev1beta2 "github.com/projectcapsule/capsule/api/v1beta2"
 )
 
+// TemplateForTenantAndNamespace applies templatingto the provided string.
+func TemplateForTenantAndNamespace(template string, tnt *capsulev1beta2.Tenant, ns *corev1.Namespace) string {
+	if !strings.Contains(template, "{{") && !strings.Contains(template, "}}") {
+		return ""
+	}
+
+	t := fasttemplate.New(template, "{{", "}}")
+
+	return t.ExecuteString(map[string]any{
+		"tenant.name": tnt.Name,
+		"namespace":   ns.Name,
+	})
+}
+
 // TemplateForTenantAndNamespace applies templating to all values in the provided map in place.
-func TemplateForTenantAndNamespace(m map[string]string, tnt *capsulev1beta2.Tenant, ns *corev1.Namespace) {
+func TemplateForTenantAndNamespaceMap(m map[string]string, tnt *capsulev1beta2.Tenant, ns *corev1.Namespace) {
 	for k, v := range m {
-		if !strings.Contains(v, "{{ ") && !strings.Contains(v, " }}") {
-			continue
-		}
-
-		t := fasttemplate.New(v, "{{ ", " }}")
-		tmplString := t.ExecuteString(map[string]any{
-			"tenant.name": tnt.Name,
-			"namespace":   ns.Name,
-		})
-
-		m[k] = tmplString
+		m[k] = TemplateForTenantAndNamespace(v, tnt, ns)
 	}
 }
