2f0adbf62744fcfb85b1c63ae13f290881c21063
Oliver Baehler <oliver@sudo-i.net>
2026-06-10 19:10:01 +0200

    feat: abstract ruling
    Signed-off-by: Oliver Baehler <oliver@sudo-i.net>

--- internal/webhook/pod/handler.go
+++ internal/webhook/pod/handler.go
@@ -6,6 +6,7 @@ package pod
 import (
 	corev1 "k8s.io/api/core/v1"
 
+	capsulev1beta2 "github.com/projectcapsule/capsule/api/v1beta2"
 	"github.com/projectcapsule/capsule/pkg/runtime/handlers"
 )
 
@@ -16,4 +17,18 @@ func Handler(handler ...handlers.TypedHandlerWithTenantWithRuleset[*corev1.Pod])
 		},
 		Handlers: handler,
 	}
+}
+
+type podRuleSet[R any] = rules.Set[R, *corev1.Pod]
+
+func evaluatePodRules[R any](
+	pod *corev1.Pod,
+	tenant *capsulev1beta2.Tenant,
+	set podRuleSet[R],
+) error {
+	if pod == nil || tenant == nil {
+		return nil
+	}
+
+	return rules.EvaluateTenantRules[R, *corev1.Pod](pod, tenant, set)
 }
