7ca98923cfd8ac3b8a99ac417c41e4fced56b033
bakito <github@bakito.ch>
2026-07-16 19:20:20 +0200

    move btg cli under break-the-glass
    Signed-off-by: bakito <github@bakito.ch>

--- cmd/cli/cmd/breaktheglass/activate.go
+++ cmd/cli/cmd/breaktheglass/activate.go
@@ -1,7 +1,7 @@
 // Copyright 2020-2026 Project Capsule Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package cmd
+package breaktheglass
 
 import (
 	"github.com/spf13/cobra"
@@ -10,20 +10,23 @@ import (
 	"github.com/projectcapsule/capsule/pkg/api/breaktheglass"
 )
 
-var expireCmd = &cobra.Command{
-	Use:   "expire",
-	Short: "expire a BreakRequest",
+var activateCmd = &cobra.Command{
+	Use:   "activate",
+	Short: "activate a BreakRequest",
 	Args:  cobra.ExactArgs(1),
 	Example: `
-  # expire an existing BreakRequest
-  capsule expire grant-admin --namespace default
+  # activate an existing BreakRequest
+  capsule activate grant-admin --namespace default
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		name = args[0]
 
 		return runBreakRequestAction(
-			func(br *capsulev1beta2.BreakRequest, user *breaktheglass.AccessEntity) error {
-				return br.ExpireRequest(user)
+			func(
+				br *capsulev1beta2.BreakRequest,
+				user *breaktheglass.AccessEntity,
+			) error {
+				return br.ActiveRequest(user)
 			},
 		)
 	},
--- cmd/cli/cmd/breaktheglass/expire.go
+++ cmd/cli/cmd/breaktheglass/expire.go
@@ -1,7 +1,7 @@
 // Copyright 2020-2026 Project Capsule Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package cmd
+package breaktheglass
 
 import (
 	"github.com/spf13/cobra"
@@ -10,23 +10,20 @@ import (
 	"github.com/projectcapsule/capsule/pkg/api/breaktheglass"
 )
 
-var activateCmd = &cobra.Command{
-	Use:   "activate",
-	Short: "activate a BreakRequest",
+var expireCmd = &cobra.Command{
+	Use:   "expire",
+	Short: "expire a BreakRequest",
 	Args:  cobra.ExactArgs(1),
 	Example: `
-  # activate an existing BreakRequest
-  capsule activate grant-admin --namespace default
+  # expire an existing BreakRequest
+  capsule expire grant-admin --namespace default
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		name = args[0]
 
 		return runBreakRequestAction(
-			func(
-				br *capsulev1beta2.BreakRequest,
-				user *breaktheglass.AccessEntity,
-			) error {
-				return br.ActiveRequest(user)
+			func(br *capsulev1beta2.BreakRequest, user *breaktheglass.AccessEntity) error {
+				return br.ExpireRequest(user)
 			},
 		)
 	},
