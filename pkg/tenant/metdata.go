3526967425fcfabf95206fbe072c620fc0e04ec1
Oliver Baehler <oliver@sudo-i.net>
2026-08-05 12:59:57 +0200

    feat: add globalresourcequota api
    Signed-off-by: Oliver Baehler <oliver@sudo-i.net>

--- pkg/tenant/metdata.go
+++ pkg/tenant/metdata.go
@@ -177,6 +177,7 @@ func BuildNamespaceAnnotationsForTenant(tnt *capsulev1beta2.Tenant) map[string]s
 			annotations[meta.AvailableIngressClassesAnnotation] = strings.Join(ic.Exact, ",")
 		}
 
+		//nolint:staticcheck
 		if len(ic.Regex) > 0 {
 			annotations[meta.AvailableIngressClassesRegexpAnnotation] = ic.Regex
 		}
@@ -187,6 +188,7 @@ func BuildNamespaceAnnotationsForTenant(tnt *capsulev1beta2.Tenant) map[string]s
 			annotations[meta.AvailableStorageClassesAnnotation] = strings.Join(sc.Exact, ",")
 		}
 
+		//nolint:staticcheck
 		if len(sc.Regex) > 0 {
 			annotations[meta.AvailableStorageClassesRegexpAnnotation] = sc.Regex
 		}
